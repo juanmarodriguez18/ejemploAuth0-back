bf0b8d4f9a21449f3a2cf8fe6fa74e280a44acd9
juanmarodriguez18 <juanm.rodriguez@alumnos.frm.utn.edu.ar>
2024-06-21 12:53:11 -0300

    subiendo ejemplo auth0 back

---
+++ src/main/java/com/example/auth0test/Auth0Test.java
@@ -0,0 +1,14 @@
+package com.example.auth0test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0Test {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0Test.class, args);
+		System.out.println("En ejecución");
+	}
+
+}
